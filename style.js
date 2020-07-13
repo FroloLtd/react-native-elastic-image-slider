aa639bc2a7f63ef7dc3b4fed7908b91cd34509bd
shubhank <shubhank@studiographene.com>
2020-07-13 17:16:11 +0530

    removed unused styles

--- style.js
+++ style.js
@@ -12,26 +12,7 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
         overflow: 'hidden'
-    },
-    image: {
-        width: Dimensions.get('window').width
-    },
-    
-    count: {
-        height: 36,
-        width: 36,
-        top: 40,
-        right: 20,
-        position: 'absolute',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(74,73,74,0.3)',
-        borderRadius: 18
-    },
-    countText: {
-        color: '#fff',
-        fontSize: 18,
-    }
+    },  
 });
 
 export default styles;
